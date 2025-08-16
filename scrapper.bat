62d3912187698d7b4f747e6acafec65acb2d7f73
Bruno Branco <brunocbranco16@gmail.com>
2025-08-16 12:47:40 -0300

    add batch file for windows

---
+++ scrapper.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Change to the desired folder:
+cd /d "C:\Users\renan\Desktop\All In All\Documentos\pos-graduacao\scrapper"
+
+REM Run the Node app
+node app.js
+
+REM Keep window open to show output/errors
+echo.
+echo Press any key to exit...
+pause >nul
